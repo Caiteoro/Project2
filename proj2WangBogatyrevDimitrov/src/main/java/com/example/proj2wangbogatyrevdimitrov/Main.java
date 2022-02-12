ecf20ac7292c429bcf4ce5f95e03f58cb40dc4d0
anton dimitrov <adimit23@colby.edu>
2022-02-11 22:17:02 -0500

    Need to update configureGoodbyeButton

--- proj2WangBogatyrevDimitrov/src/main/java/com/example/proj2wangbogatyrevdimitrov/Main.java
+++ proj2WangBogatyrevDimitrov/src/main/java/com/example/proj2wangbogatyrevdimitrov/Main.java
@@ -16,15 +16,9 @@ import javafx.scene.control.TextInputDialog;
 
 
 public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Anton, Baron, and Philipp's Project 2");
-        stage.setScene(scene);
-
-        stage.show();
-        Button hello = (Button) scene.lookup("#hello");
+	
+	public void configureHelloButton(Scene scene){
+		Button hello = (Button) scene.lookup("#hello");
         hello.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -35,20 +29,47 @@ public class Main extends Application {
                 result.ifPresent(number -> {hello.setText(number);});
             }
         });
-
-        Button goodbye = (Button) scene.lookup("#goodbye");
-
+	}
+	
+	public void configureGoodbyeButton(Scene scene){
+		Button goodbye = (Button) scene.lookup("#goodbye");
+	}
+	
+	public void configureMenuBar(Scene scene){
+		
+		Button hello = (Button) scene.lookup("#hello");
         TextArea textArea = (TextArea) scene.lookup("#textarea");
-
-
-        MenuBar menubar = (MenuBar) scene.lookup("#menubar");
-
-        Menu menu = (Menu) menubar.getMenus().get(0);
-
-        MenuItem reset = (MenuItem) menu.getItems().get(0);
-
+		MenuBar menubar = (MenuBar) scene.lookup("#menubar");
+		Menu menu = (Menu) menubar.getMenus().get(0);
+		
+        MenuItem reset = (MenuItem) menu.getItems().get(0);	
+		reset.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                textArea.setText("Sample text");
+                hello.setText("Hello");
+            }
+        });	
+		
+		MenuItem exit = (MenuItem) menu.getItems().get(1);
+		exit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });	
+	}
+	
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Anton, Baron, and Philipp's Project 2");
+        stage.setScene(scene);
+        stage.show();
+        
+		configureHelloButton(scene);
+		configureGoodbyeButton(scene);
+        configureMenuBar(scene);
     }
-
     public static void main(String[] args) {
         launch();
     }
